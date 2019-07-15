20fb623cff42b11601730090ecdd2e75297bc948
Dean Lovett <dean.lovett@gmail.com>
2019-07-15 14:44:47 -0700

    adds circlci

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 //mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
-mongoose.connect('mongodb://localhost/my-blog');
+//mongoose.connect('mongodb://localhost/my-blog');
+// mongodb://<dbuser>:<dbpassword>@ds133137.mlab.com:33137/heroku_fxdbq06m
+
+mongoose.connect(
+    'mongodb://test:test12@ds133137.mlab.com:33137/heroku_fxdbq06m',
+    { useNewUrlParser: true }
+);
+
 mongoose.Promise = Promise;
 
 const app = express();
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const server = require('./app');
-//require('dotenv').config();
-const PORT = process.env.PORT || 8080;
+require('dotenv').config();
+
+const PORT = process.env.PORT;
 
 server.listen(PORT, function() {
   console.log(`Server is listening on http://localhost:${PORT}`);
